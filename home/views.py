97d032be17ef02008e8b0d63617e541c26e5b9fd
Crowdbotics <team@crowdbotics.com>
2019-01-21 18:12:21 +0000

    remove package twilio

--- home/views.py
+++ home/views.py
@@ -7,7 +7,6 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
-	{'name':'twilio', 'url': 'http://pypi.python.org/pypi/twilio/6.10.1'},
 	{'name':'pinax-notifications', 'url': 'http://pypi.python.org/pypi/pinax-notifications/5.0.1'},
 	{'name':'django-todo', 'url': 'http://pypi.python.org/pypi/django-todo/1.6.2'},
 	{'name':'wq', 'url': 'http://pypi.python.org/pypi/wq/1.0.0'},
