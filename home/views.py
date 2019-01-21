bbfc70ed8bf5c94afc2d8d4eaf79640eb6eb3e2f
Crowdbotics <team@crowdbotics.com>
2019-01-21 18:12:46 +0000

    remove package django-file-upload

--- home/views.py
+++ home/views.py
@@ -10,7 +10,6 @@ def home(request):
 	{'name':'pinax-notifications', 'url': 'http://pypi.python.org/pypi/pinax-notifications/5.0.1'},
 	{'name':'django-todo', 'url': 'http://pypi.python.org/pypi/django-todo/1.6.2'},
 	{'name':'wq', 'url': 'http://pypi.python.org/pypi/wq/1.0.0'},
-	{'name':'django-file-upload', 'url': 'http://pypi.python.org/pypi/django-file-upload/1.0.0'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
     ]
     context = {
