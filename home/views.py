97967b9d60f726e0e9330df010212c2d6a0554ed
Crowdbotics <team@crowdbotics.com>
2019-01-21 18:13:49 +0000

    remove package wq

--- home/views.py
+++ home/views.py
@@ -7,7 +7,6 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
-	{'name':'wq', 'url': 'http://pypi.python.org/pypi/wq/1.0.0'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
     ]
     context = {
